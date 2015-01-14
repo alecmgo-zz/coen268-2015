50c3d1345d34bae143852eed277e4b3ba6a888a1
Alec Go <alecmgo@gmail.com>
2015-01-14 09:18:32 -0800

    Lecture 4 code.

---
+++ c04.views/src/main/java/edu/scu/c04views/EditTextActivity.java
@@ -0,0 +1,17 @@
+package edu.scu.c04views;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class EditTextActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text_activity);
+    }
+
+}
